757340a170f50294892babc8fa98c1f7ba91925e
Jemy Chen <jemychen@live.com>
2020-01-22 23:46:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,7 +45,10 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+  total = 0
+  list_of_directors = list_of_directors(source)
+  directors_totals = directors_totals(source)
+  }
 end
 
 
